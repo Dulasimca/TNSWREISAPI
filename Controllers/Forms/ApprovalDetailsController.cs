8be6251156528098345044bf95a6cc402e892b44
Aditiya_T661 <Aditiya_T661@TANDVCL0041>
2021-12-08 20:28:10 +0530

    approvallist,details,registration changes added

---
+++ Controllers/Forms/ApprovalDetailsController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using TNSWREISAPI.ManageSQL;
+
+namespace TNSWREISAPI.Controllers.Forms
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApprovalDetailsController : ControllerBase
+    {
+        [HttpPost("{id}")]
+
+        public string Post(ApprovalDetailsEntity ApprovalDetailsEntity)
+        {
+            try
+            {
+                ManageSQLConnection manageSQL = new ManageSQLConnection();
+                List<KeyValuePair<string, string>> sqlParameters = new List<KeyValuePair<string, string>>();
+                sqlParameters.Add(new KeyValuePair<string, string>("@Id", Convert.ToString(ApprovalDetailsEntity.FacilityDetailId)));
+                sqlParameters.Add(new KeyValuePair<string, string>("@HostelID", Convert.ToString(ApprovalDetailsEntity.HostelID)));
+                sqlParameters.Add(new KeyValuePair<string, string>("@Districtcode", Convert.ToString(ApprovalDetailsEntity.Districtcode)));
+                sqlParameters.Add(new KeyValuePair<string, string>("@Talukid", Convert.ToString(ApprovalDetailsEntity.Talukid)));
+                sqlParameters.Add(new KeyValuePair<string, string>("@ApprovalType", Convert.ToString(ApprovalDetailsEntity.ApprovalType)));
+                sqlParameters.Add(new KeyValuePair<string, string>("@RequestId", Convert.ToString(ApprovalDetailsEntity.RequestId)));
+                sqlParameters.Add(new KeyValuePair<string, string>("@Flag", Convert.ToString(ApprovalDetailsEntity.Flag)));
+                var result = manageSQL.InsertData("InsertApprovalDetails", sqlParameters);
+                return JsonConvert.SerializeObject(result);
+            }
+            catch (Exception ex)
+            {
+                AuditLog.WriteError(ex.Message);
+            }
+            return "false";
+        }
+    }
+    public class ApprovalDetailsEntity
+    {
+        public int FacilityDetailId { get; set; }
+        public int HostelID { get; set; }
+        public int Districtcode { get; set; }
+        public int Talukid { get; set; }
+        public int ApprovalType { get; set; }
+        public int RequestId { get; set; }
+        public bool Flag { get; set; }
+    }
+}
---
+++ Controllers/Forms/ApprovalListController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using TNSWREISAPI.ManageSQL;
+
+namespace TNSWREISAPI.Controllers.Forms
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApprovalListController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            ManageSQLConnection manageSQL = new ManageSQLConnection();
+            DataSet ds = new DataSet();
+            ds = manageSQL.GetDataSetValues("GetApprovallist");
+            return JsonConvert.SerializeObject(ds.Tables[0]);
+
+        }
+    }
+}
