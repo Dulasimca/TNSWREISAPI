218d37d3b0045e85c89573fe5e81ad0058ad2d2a
chandrika31 <udhayachandrikamurugan94@gmail.com>
2022-01-24 12:55:56 +0530

    student biometric controller

---
+++ Controllers/Forms/StudentBiometricDetailsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TNSWREISAPI.Controllers.Forms
+{
+    public class StudentBiometricDetailsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
