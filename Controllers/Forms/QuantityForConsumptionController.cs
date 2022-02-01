5e4830e1f67f38ed41e76a6f05164b36c8616a3e
chandrika31 <udhayachandrikamurugan94@gmail.com>
2022-02-01 11:42:10 +0530

    consumption changes

--- Controllers/Forms/QuantityForConsumptionController.cs
+++ Controllers/Forms/QuantityForConsumptionController.cs
@@ -14,13 +14,14 @@ namespace TNSWREISAPI.Controllers.Forms
     public class QuantityForConsumptionController : ControllerBase
     {
         [HttpGet("{id}")]
-        public string Get(string Commodity, string AccountingYear)
+        public string Get(string Commodity, string AccountingYear, string Date)
         {
             ManageSQLConnection manageSQL = new ManageSQLConnection();
             DataSet ds = new DataSet();
             List<KeyValuePair<string, string>> sqlParameters = new List<KeyValuePair<string, string>>();
                 sqlParameters.Add(new KeyValuePair<string, string>("@Commodity", Commodity));
                 sqlParameters.Add(new KeyValuePair<string, string>("@AccountingYear", AccountingYear));
+            sqlParameters.Add(new KeyValuePair<string, string>("@Date", Date));
             ds = manageSQL.GetDataSetValues("GetCommodityQtyForConsumption", sqlParameters);
             return JsonConvert.SerializeObject(ds.Tables[0]);
         }
