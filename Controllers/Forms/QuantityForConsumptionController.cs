32cef5b50347e05c6403f1de3910b005d8e5dcd8
chandrika31 <udhayachandrikamurugan94@gmail.com>
2022-02-01 18:04:36 +0530

    consumption qty get api

--- Controllers/Forms/QuantityForConsumptionController.cs
+++ Controllers/Forms/QuantityForConsumptionController.cs
@@ -14,15 +14,24 @@ namespace TNSWREISAPI.Controllers.Forms
     public class QuantityForConsumptionController : ControllerBase
     {
         [HttpGet("{id}")]
-        public string Get(string Commodity, string AccountingYear, string Date)
+        public string Get(string Commodity, string Code, string Date, int Type)
         {
             ManageSQLConnection manageSQL = new ManageSQLConnection();
             DataSet ds = new DataSet();
             List<KeyValuePair<string, string>> sqlParameters = new List<KeyValuePair<string, string>>();
+            var procedure = string.Empty;
                 sqlParameters.Add(new KeyValuePair<string, string>("@Commodity", Commodity));
-                sqlParameters.Add(new KeyValuePair<string, string>("@AccountingYear", AccountingYear));
+            if (Type == 1)
+            {
+                procedure = "GetOBUptoDay";
+                sqlParameters.Add(new KeyValuePair<string, string>("@HCode", Code));
+            }
+            else
+            {
+                procedure = "GetCommodityQtyForConsumption";
+            }
             sqlParameters.Add(new KeyValuePair<string, string>("@Date", Date));
-            ds = manageSQL.GetDataSetValues("GetCommodityQtyForConsumption", sqlParameters);
+            ds = manageSQL.GetDataSetValues(procedure, sqlParameters);
             return JsonConvert.SerializeObject(ds.Tables[0]);
         }
 
