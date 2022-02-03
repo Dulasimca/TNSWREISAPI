61193bcaf470136cbb068cf4d72abc87ddba05b4
Aditiya_T661 <Aditiya_T661@TANDVCL0041>
2022-02-03 11:18:04 +0530

    attendanceBMName cont added

---
+++ Controllers/BioMetric/AttendanceBMNameController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TNSWREISAPI.ManageSQL;
+using System.Data;
+
+namespace TNSWREISAPI.Controllers.BioMetric
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AttendanceBMNameController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public string Get(string Adate, string HostelId)
+        {
+            ManageBioMetricsConnection manageSQL = new ManageBioMetricsConnection();
+            List<KeyValuePair<string, string>> sqlParameters = new List<KeyValuePair<string, string>>();
+            sqlParameters.Add(new KeyValuePair<string, string>("@Adate", Adate));
+            sqlParameters.Add(new KeyValuePair<string, string>("@HostelId", HostelId));
+            var result = manageSQL.GetDataSetValues("GetBiometricDeviceAttendanceName", sqlParameters);
+            return JsonConvert.SerializeObject(result);
+
+        }
+    }
+}
