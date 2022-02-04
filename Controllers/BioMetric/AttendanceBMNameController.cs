b83096d366a0093dc1fe33797d549ac64e242635
srikanth2419 <srikanthraj.raj@gmail.com>
2022-02-04 17:49:09 +0530

    changes

--- Controllers/BioMetric/AttendanceBMNameController.cs
+++ Controllers/BioMetric/AttendanceBMNameController.cs
@@ -15,10 +15,12 @@ namespace TNSWREISAPI.Controllers.BioMetric
     public class AttendanceBMNameController : ControllerBase
     {
         [HttpGet("{id}")]
-        public string Get(string Adate, string HostelId)
+        public string Get(string DCode, string TCode, string Adate, string HostelId)
         {
             ManageBioMetricsConnection manageSQL = new ManageBioMetricsConnection();
             List<KeyValuePair<string, string>> sqlParameters = new List<KeyValuePair<string, string>>();
+            sqlParameters.Add(new KeyValuePair<string, string>("@DCode", DCode));
+            sqlParameters.Add(new KeyValuePair<string, string>("@TCode", TCode));
             sqlParameters.Add(new KeyValuePair<string, string>("@Adate", Adate));
             sqlParameters.Add(new KeyValuePair<string, string>("@HostelId", HostelId));
             var result = manageSQL.GetDataSetValues("GetBiometricDeviceAttendanceName", sqlParameters);
