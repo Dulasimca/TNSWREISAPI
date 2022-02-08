70976e94934a08843a8f683caa0d76f4217db826
Aditiya_T661 <Aditiya_T661@TANDVCL0041>
2022-02-08 17:46:25 +0530

    devicemappingreport added

---
+++ Controllers/Reports/DeviceMappingReportController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using TNSWREISAPI.ManageSQL;
+using TNSWREISAPI.Model;
+
+namespace TNSWREISAPI.Controllers.Reports
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DeviceMappingReportController : ControllerBase
+    {
+        [HttpGet("{id}")]
+
+        public string Get(int DCode, int TCode, int HostelId)
+        {
+            ManageSQLConnection manageSQL = new ManageSQLConnection();
+            DataSet ds = new DataSet();
+            List<KeyValuePair<string, string>> sqlParameters = new List<KeyValuePair<string, string>>();
+            sqlParameters.Add(new KeyValuePair<string, string>("@Id", Convert.ToString(DCode)));
+            sqlParameters.Add(new KeyValuePair<string, string>("@TCode", Convert.ToString(TCode)));
+            sqlParameters.Add(new KeyValuePair<string, string>("@HCode", Convert.ToString(HostelId)));
+            var result = manageSQL.GetDataSetValues("GetBiometricDeviceMappingReport", sqlParameters);
+            return JsonConvert.SerializeObject(result);
+        }
+    }
+}
