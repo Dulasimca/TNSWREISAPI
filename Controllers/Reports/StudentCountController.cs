fc191683ff199f676a07fcce4c965383bbe698b4
VishnupriyaMurugan <vishnupriyatalks@gmail.com>
2022-03-16 12:06:08 +0530

    student count

---
+++ Controllers/Reports/StudentCountController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using TNSWREISAPI.ManageSQL;
+
+
+namespace TNSWREISAPI.Controllers.Reports
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentCountController : Controller
+    {
+        [HttpGet]
+        public string Get()
+        {
+            ManageSQLConnection manageSQL = new ManageSQLConnection();
+            DataSet ds = new DataSet();
+            ds = manageSQL.GetDataSetValues("GetStudentsCount");
+            return JsonConvert.SerializeObject(ds.Tables[0]);
+        }
+    }
+}
