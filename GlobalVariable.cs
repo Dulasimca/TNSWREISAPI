d0db343a847697d7f8886833bb0719eb912cfad4
Dulasimca <dulasimca@gmail.com>
2022-05-06 17:51:42 +0530

    changes added

--- GlobalVariable.cs
+++ GlobalVariable.cs
@@ -8,16 +8,15 @@ namespace TNSWREISAPI
     public class GlobalVariable
     {
         //Testing connection
-        // public const string ConnectionString = "data source=180.179.49.72;initial catalog=TNSWREIS;user id = sqladmin; password =sql@svc&ac!72;";
-        //public const string ConnectionStringBiometric = "data source=180.179.49.72;initial catalog=etimetracklite1;user id = sqladmin; password =sql@svc&ac!72;";
-        // public const string ConnectionStringForFaceReadingPostgreSQL = "data source=180.179.49.72;initial catalog=etimetracklite1;user id = sqladmin; password =sql@svc&ac!72;";
-        //public const string FolderPath = "E://Angular//TNSWREIS//src//assets//layout//";
+        public const string ConnectionString = "data source=183.82.34.129;initial catalog=TNSWREIS;user id = sqladmin; password =sql@svc&ac!147;";
+        public const string ConnectionStringBiometric = "data source=183.82.34.129;initial catalog=etimetracklite1;user id = sqladmin; password =sql@svc&ac!147;";
+        public const string ConnectionStringForFaceReadingPostgreSQL = "data source=183.82.34.129;initial catalog=etimetracklite1;user id = sqladmin; password =sql@svc&ac!147;";
+        public const string FolderPath = "E://Angular//TNSWREIS//src//assets//layout//";
 
         //Live connection
-        public const string ConnectionString = "data source=localhost;initial catalog=TNSWREIS;user id =sqladmin; password =sql@svc&ac!72;";
-        public const string ConnectionStringBiometric = "data source=localhost;initial catalog=etimetracklite1;user id = sqladmin; password =sql@svc&ac!72;";
-        public const string ConnectionStringForFaceReadingPostgreSQL = "data source=localhost;initial catalog=etimetracklite1;user id = sqladmin; password =sql@svc&ac!72;";
-        public const string FolderPath = "C://LocalRepository//TestingSite//TNADWUI//assets//layout//";
-  
+        //public const string ConnectionString = "data source=localhost;initial catalog=TNADWHMS;user id =sa; password =Tryme@43$#7&!2;";
+        //public const string ConnectionStringBiometric = "data source=localhost;initial catalog=etimetracklite1;user id = sa; password =Tryme@43$#7&!2;";
+        //public const string ConnectionStringForFaceReadingPostgreSQL = "data source=localhost;initial catalog=etimetracklite1;user id = sa; password =Tryme@43$#7&!2;";
+        //public const string FolderPath = "D://Respository//Live//TNADWUI//assets//layout//";
     }
 }
---
+++ Model/ManageCMDashboardData.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace TNSWREISAPI.Model
+{
+    public class ManageCMDashboardData
+    {
+
+        public List<CMDasshboardEntity> ManageDashBoardData(DataSet ds)
+        {
+            List<CMDasshboardEntity> _DashBoardData = new List<CMDasshboardEntity>();
+            
+            try
+            {
+                if (ds.Tables.Count > 1)
+                {
+                    foreach (DataRow ManageData in ds.Tables[1].Rows) // District/Taluk/Hostel
+                    {
+
+                        DataRow[] FilteredData = ds.Tables[0].Select("Code='" + Convert.ToString(ManageData["Code"]) + "'");
+                        CMDasshboardEntity _Data = new CMDasshboardEntity();
+                        _Data.name = Convert.ToString(FilteredData[0]["Name"]);
+                        _Data.Id = Convert.ToInt32(FilteredData[0]["Code"]);
+                        foreach (DataRow nFData in FilteredData)
+                        {
+                            _Data.hcount += Convert.ToInt32(nFData["HCount"]);
+                            if(Convert.ToInt32(nFData["HGenderType"])==1) //Boys
+                            {
+                                _Data.boysHostelCount = Convert.ToInt32(nFData["HCount"]);
+                                    _Data.sanctionedBoysCount = Convert.ToInt32(nFData["sanctionedStength"]);
+                                _Data.boysCount = Convert.ToInt32(nFData["StudentCount"]);
+                            }
+                            else if(Convert.ToInt32(nFData["HGenderType"]) == 2) //Girls
+                            {
+                                _Data.girlsHostelCount = Convert.ToInt32(nFData["HCount"]);
+                                _Data.sanctionedGirlsCount = Convert.ToInt32(nFData["sanctionedStength"]);
+                                _Data.girlsCount = Convert.ToInt32(nFData["StudentCount"]);
+                            }
+                            else //Others
+                            {
+                                _Data.boysHostelCount = Convert.ToInt32(nFData["HCount"]);
+                                _Data.sanctionedBoysCount = Convert.ToInt32(nFData["sanctionedStength"]);
+                                _Data.boysCount = Convert.ToInt32(nFData["StudentCount"]);
+                            }
+                        }
+                        _DashBoardData.Add(_Data);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                AuditLog.WriteError(ex.Message);
+            }
+            return _DashBoardData;
+        }
+    }
+
+    public class CMDasshboardEntity
+    {
+        public string name { get; set; }
+        public int Id { get; set; }
+        public int hcount { get; set; }
+        public int boysHostelCount { get; set; }
+        public int girlsHostelCount { get; set; }
+        public int sanctionedBoysCount { get; set; }
+        public int sanctionedGirlsCount { get; set; }
+        public int boysCount { get; set; }
+        public int girlsCount { get; set; }
+    }
+}
