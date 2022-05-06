74b5695e5c2df5ab847b854cb7061054c0a73fc1
Dulasimca <dulasimca@gmail.com>
2022-05-06 17:57:16 +0530

    changes added

--- Controllers/CMDashboard/CMMainDashBoard.cs
+++ Controllers/CMDashboard/CMMainDashBoard.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TNSWREISAPI.ManageSQL;
+using TNSWREISAPI.Model;
 
 namespace TNSWREISAPI.Controllers.CMDashboard
 {
@@ -12,7 +15,23 @@ namespace TNSWREISAPI.Controllers.CMDashboard
     public class CMMainDashBoard : ControllerBase
     {
 
-
+        [HttpGet]
+        public string Get()
+        {
+            try
+            {
+                ManageSQLConnection manageSQL = new ManageSQLConnection();
+                var result = manageSQL.GetDataSetValues("GetOveralldetails");
+                ManageCMDashboardData cMDashboardData = new ManageCMDashboardData();
+                var nData = cMDashboardData.ManageDashBoardData(result);
+                return JsonConvert.SerializeObject(nData);
+            }
+            catch (Exception ex)
+            {
+                AuditLog.WriteError(ex.Message);
+                return "";
+            }
+        }
 
     }
 }
