ae12378e110af1a0975f1dc81ba281ea2c737c20
srikanth2419 <srikanthraj.raj@gmail.com>
2022-05-09 17:27:21 +0530

    changes in API

--- GlobalVariable.cs
+++ GlobalVariable.cs
@@ -8,16 +8,16 @@ namespace TNSWREISAPI
     public class GlobalVariable
     {
         //Testing connection
-        // public const string ConnectionString = "data source=180.179.49.72;initial catalog=TNSWREIS;user id = sqladmin; password =sql@svc&ac!72;";
-        //public const string ConnectionStringBiometric = "data source=180.179.49.72;initial catalog=etimetracklite1;user id = sqladmin; password =sql@svc&ac!72;";
-        // public const string ConnectionStringForFaceReadingPostgreSQL = "data source=180.179.49.72;initial catalog=etimetracklite1;user id = sqladmin; password =sql@svc&ac!72;";
-        //public const string FolderPath = "E://Angular//TNSWREIS//src//assets//layout//";
+        public const string ConnectionString = "data source=180.179.49.72;initial catalog=TNSWREIS;user id = sqladmin; password =sql@svc&ac!72;";
+        public const string ConnectionStringBiometric = "data source=180.179.49.72;initial catalog=etimetracklite1;user id = sqladmin; password =sql@svc&ac!72;";
+        public const string ConnectionStringForFaceReadingPostgreSQL = "data source=180.179.49.72;initial catalog=etimetracklite1;user id = sqladmin; password =sql@svc&ac!72;";
+        public const string FolderPath = "E://Angular//TNSWREIS//src//assets//layout//";
 
-        //Live connection
-        public const string ConnectionString = "data source=localhost;initial catalog=TNSWREIS;user id =sqladmin; password =sql@svc&ac!72;";
-        public const string ConnectionStringBiometric = "data source=localhost;initial catalog=etimetracklite1;user id = sqladmin; password =sql@svc&ac!72;";
-        public const string ConnectionStringForFaceReadingPostgreSQL = "data source=localhost;initial catalog=etimetracklite1;user id = sqladmin; password =sql@svc&ac!72;";
-        public const string FolderPath = "C://LocalRepository//TestingSite//TNADWUI//assets//layout//";
+       //Live connection
+       // public const string ConnectionString = "data source=localhost;initial catalog=TNSWREIS;user id =sqladmin; password =sql@svc&ac!72;";
+       // public const string ConnectionStringBiometric = "data source=localhost;initial catalog=etimetracklite1;user id = sqladmin; password =sql@svc&ac!72;";
+       // public const string ConnectionStringForFaceReadingPostgreSQL = "data source=localhost;initial catalog=etimetracklite1;user id = sqladmin; password =sql@svc&ac!72;";
+      //  public const string FolderPath = "C://LocalRepository//TestingSite//TNADWUI//assets//layout//";
   
     }
 }
