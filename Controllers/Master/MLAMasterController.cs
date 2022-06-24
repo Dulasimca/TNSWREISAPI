5d09acab3a7d6d5035215c88f782db21b5b58592
chandrika31 <udhayachandrikamurugan94@gmail.com>
2022-06-24 17:44:23 +0530

    hostel master changes in insert (mla, mp) and mla, mp masters added

---
+++ Controllers/Master/MLAMasterController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using TNSWREISAPI.ManageSQL;
+
+namespace TNSWREISAPI.Controllers.Master
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MLAMasterController : Controller
+    {
+        [HttpGet("{id}")]
+        public string Get(int MPId)
+        {
+            ManageSQLConnection manageSQL = new ManageSQLConnection();
+            DataSet ds = new DataSet();
+            List<KeyValuePair<string, string>> sqlParameters = new List<KeyValuePair<string, string>>();
+            sqlParameters.Add(new KeyValuePair<string, string>("@MPId", Convert.ToString(MPId)));
+            ds = manageSQL.GetDataSetValues("GetMLAConstituency", sqlParameters);
+            return JsonConvert.SerializeObject(ds.Tables[0]);
+        }
+
+    }
+}
---
+++ Controllers/Master/MPMasterController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using TNSWREISAPI.ManageSQL;
+
+namespace TNSWREISAPI.Controllers.Master
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MPMasterController : Controller
+    {
+        [HttpGet]
+        public string Get()
+        {
+            ManageSQLConnection manageSQL = new ManageSQLConnection();
+            DataSet ds = new DataSet();
+            ds =  manageSQL.GetDataSetValues("GetMPConstituency");
+            return JsonConvert.SerializeObject(ds.Tables[0]);
+        }
+    }
+}
