63965b7d539ecb224b1b652999e7fe3ffdc7684a
Adithya_10010 <Adithya_10010@TANDVBL0037>
2022-06-30 15:46:32 +0530

    changes added

--- Controllers/Forms/EmployeeDetailsController.cs
+++ Controllers/Forms/EmployeeDetailsController.cs
@@ -42,8 +42,9 @@ namespace TNSWREISAPI.Controllers.Forms
                 sqlParameters.Add(new KeyValuePair<string, string>("@EmployeeImage", Convert.ToString(EmployeeDetailsEntity.EmployeeImage)));
                 sqlParameters.Add(new KeyValuePair<string, string>("@DOB", Convert.ToString(EmployeeDetailsEntity.DOB)));
                 sqlParameters.Add(new KeyValuePair<string, string>("@Flag", Convert.ToString(EmployeeDetailsEntity.Flag)));
+                sqlParameters.Add(new KeyValuePair<string, string>("@DApproval", Convert.ToString(EmployeeDetailsEntity.DApproval)));
                 var result = manageSQL.InsertData("InsertEmployeeDetails", sqlParameters);
-                return JsonConvert.SerializeObject(result);
+                return JsonConvert.SerializeObject(result); 
             }
             catch (Exception ex)
             {
@@ -91,6 +92,7 @@ namespace TNSWREISAPI.Controllers.Forms
         public bool Flag { get; set; }
         public string EmployeeImage { get; set; }
         public DateTime DOB { get; set; }
+        public int DApproval { get; set; }
 
     }
 }
