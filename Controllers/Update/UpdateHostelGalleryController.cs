3424215165e11507a5afad497a18a3af98b71a70
Adithya_10010 <Adithya_10010@TANDVBL0037>
2022-07-04 18:37:36 +0530

    changes added

---
+++ Controllers/Update/UpdateHostelGalleryController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using TNSWREISAPI.ManageSQL;
+using TNSWREISAPI.Model;
+
+namespace TNSWREISAPI.Controllers.Update
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UpdateHostelGalleryController : Controller
+    {
+        [HttpPost("{id}")]
+        public bool Post(UpdateHostelGalleryEntity entity)
+        {
+            try
+            {
+                ManageSQLConnection manageSQL = new ManageSQLConnection();
+                List<KeyValuePair<string, string>> sqlParameters = new List<KeyValuePair<string, string>>();
+                sqlParameters.Add(new KeyValuePair<string, string>("@Id", Convert.ToString(entity.ImageId)));
+                var result = manageSQL.UpdateValues("UpdateHostelGallery", sqlParameters);
+                return result;
+            }
+            catch (Exception ex)
+            {
+                AuditLog.WriteError(ex.Message);
+                return false;
+            }
+
+        }
+    }
+    public class UpdateHostelGalleryEntity
+    {
+        public long ImageId { get; set; }
+    }
+}
