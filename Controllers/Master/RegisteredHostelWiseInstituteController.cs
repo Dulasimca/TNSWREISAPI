3e88ab43253a7418b199f9568e1040b9a5983520
VishnupriyaMurugan <vishnupriyatalks@gmail.com>
2022-07-15 15:09:15 +0530

    changes

---
+++ Controllers/Master/RegisteredHostelWiseInstituteController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using TNSWREISAPI.ManageSQL;
+using Newtonsoft.Json;
+
+namespace TNSWREISAPI.Controllers.Master
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegisteredHostelWiseInstituteController : Controller
+    {
+        
+            [HttpGet("{id}")]
+            public string Get(int HCode)
+            {
+                ManageSQLConnection manageSQL = new ManageSQLConnection();
+                DataSet ds = new DataSet();
+                List<KeyValuePair<string, string>> sqlParameters = new List<KeyValuePair<string, string>>();
+                sqlParameters.Add(new KeyValuePair<string, string>("@HCode", Convert.ToString(HCode)));
+                ds = manageSQL.GetDataSetValues("GetOnlineRegistrationByIType", sqlParameters);
+                return JsonConvert.SerializeObject(ds.Tables[0]);
+        }
+    }
+}
---
+++ Controllers/Update/UpdateOnlineRegistrationStatusController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using TNSWREISAPI.ManageSQL;
+using TNSWREISAPI.Model;
+
+namespace TNSWREISAPI.Controllers.Update
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UpdateOnlineRegistrationStatusController : Controller
+    {
+        [HttpPost("{id}")]
+        public string Post(StudentDetailsEntity entity)
+        {
+            ManageSQLConnection manageSQL = new ManageSQLConnection();
+            DataSet ds = new DataSet();
+            List<KeyValuePair<string, string>> sqlParameters = new List<KeyValuePair<string, string>>();
+            sqlParameters.Add(new KeyValuePair<string, string>("@StudentId", Convert.ToString(entity.StudentId)));
+            sqlParameters.Add(new KeyValuePair<string, string>("@Flag", Convert.ToString(entity.Flag)));
+            var result = manageSQL.UpdateValues("UpdateOnlineStudentRegStatus", sqlParameters);
+            return JsonConvert.SerializeObject(result);
+        }
+    }
+
+    public class StudentDetailsEntity
+    {
+        public int StudentId { get; set; }
+        public int Flag { get; set; }
+    }
+}
